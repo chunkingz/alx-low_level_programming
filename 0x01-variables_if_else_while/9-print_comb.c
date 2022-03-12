48087fa6c1ce363ed607f48dd9f7103237c8fd4f
chunkingz <fortuneking33@yahoo.com>
2022-03-11 20:12:23 -0800

    8 and 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program prints the lowercase
+ * alphabet in reverse, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+}
