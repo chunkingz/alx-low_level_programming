a4cbe0c04726ae3ce55bfa4401c0e169ba003cbf
chunkingz <fortuneking33@yahoo.com>
2022-03-11 19:59:16 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Program prints the alphabet in lowercase,
+ * followed by a new line, except q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
